efd3a507acdd6466522e49c086f3ead4a357062d
Ali Rida Awad <ali14.3awad@gmail.com>
2026-08-26 16:16:34 +0300

    Add field to Project domain entity

--- src/main/java/com/craete/vault/Domain/Projects/Entities/Project.java
+++ src/main/java/com/craete/vault/Domain/Projects/Entities/Project.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.craete.vault.Domain.Fields.Entities.Field;
 import com.craete.vault.Domain.ProjectPictures.Entities.ProjectPicture;
 import com.craete.vault.Domain.Users.Entities.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.OrderBy;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -50,6 +54,11 @@ public class Project {
     @OneToOne(optional = false)
     @JoinColumn(name = "supervisor_id", nullable = false, updatable = true)
     private User supervisor;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "project_id", nullable = false)
+    @NotNull
+    private Field field;
 
     @OneToMany(
         mappedBy = "project",
